7139fba5a7b8d99c54b661c47b78c9e9b54fa4be
naynon769-collab <naynon769@gmail.com>
2026-03-08 21:12:58 +0700

    Update Android Dpi or Resolution.bat

--- Android Dpi or Resolution.bat
+++ Android Dpi or Resolution.bat
@@ -10,7 +10,7 @@ echo 1. Change DPI
 echo 2. Change Resolution
 echo 3. Exit
 
-set /p "%option%"=="2"(720):  
+set /p "%option%"=="2":  
 
 if "%option%"=="1" (
     set /p dpi=Enter the default DPI value for your device: 
