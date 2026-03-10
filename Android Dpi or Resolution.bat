64e99e87243e95382d63cf41ce368455b19b246c
naynon769-collab <naynon769@gmail.com>
2026-03-11 00:42:19 +0700

    Update Android Dpi or Resolution.bat

--- Android Dpi or Resolution.bat
+++ Android Dpi or Resolution.bat
@@ -18,7 +18,7 @@ if "%option%"=="1" (
     set /p width= 720 Enter the width in pixels: 
     set /p height= 720 height in pixels:
     adb shell wm size width 1920 x 2940 height
-    echo Resolution set to width 900 x 1000 height.
+    echo Resolution set to width 540 x 1200 height.
     goto menu
 ) else if "%option%"=="3" (
     echo Exiting...
