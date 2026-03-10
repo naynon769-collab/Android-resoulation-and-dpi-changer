121ef69724786d41db9c95b46532ee2d52cc8dcb
naynon769-collab <naynon769@gmail.com>
2026-03-10 12:25:10 +0700

    Update Android Dpi or Resolution.bat

--- Android Dpi or Resolution.bat
+++ Android Dpi or Resolution.bat
@@ -27,7 +27,7 @@ goto menu
 :change_size
 cls
 echo [Tip] Enter WidthxHeight (e.g., 1080x1920)
-set /p w_h="Enter resolution: "
+set /p w_h="Enter resolution: "1920x2040
 adb shell wm size %w_h%
 echo [Success] Resolution set to %w_h%
 pause
